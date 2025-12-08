c44846bb6fbd9ec1ba4a1816b1c5ef30c18ef154
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-12-08 09:52:50 -0500

    test

--- app/(home)/client.ts
+++ app/(home)/client.ts
@@ -1,63 +1,68 @@
 import axios from "axios";
-const axiosWithCredentials = axios.create({ withCredentials: true });
-/* eslint-disable @typescript-eslint/no-explicit-any */
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
 
+const axiosWithCredentials = axios.create({ 
+    withCredentials: true,
+    baseURL: HTTP_SERVER,
+ });
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 export const getAllPosts = async () => {
-    const response = await axios.get(`${HTTP_SERVER}/api/posts`);
+    const response = await axios.get(`/api/posts`);
     return response.data;
 }
 export const getPostsByUserId = async (userId: string) => {
-    const response = await axios.get(`${HTTP_SERVER}/api/posts/user/${userId}`);
+    const response = await axios.get(`/api/posts/user/${userId}`);
     return response.data;
 }
 export const getPostsByPlaceId = async (placeId: string) => {
-    const response = await axios.get(`${HTTP_SERVER}/api/posts/place/${placeId}`);
+    const response = await axios.get(`/api/posts/place/${placeId}`);
     return response.data;
 }
 export const createPost = async (postData: any) => {
-    const response = await axiosWithCredentials.post(`${HTTP_SERVER}/api/posts`, postData);
+    const response = await axiosWithCredentials.post(`/api/posts`, postData);
     return response.data;
 }
 export const signup = async (userData: any) => {
-    const response = await axiosWithCredentials.post(`${HTTP_SERVER}/api/users/signup`, userData);
+    const response = await axiosWithCredentials.post(`/api/users/signup`, userData);
     return response.data;
 }
 export const signupAdmin = async (userData: any) => {
-    const response = await axiosWithCredentials.post(`${HTTP_SERVER}/api/users/signup-admin`, userData);
+    const response = await axiosWithCredentials.post(`/api/users/signup-admin`, userData);
     return response.data;
 }
 export const signin = async (userData: any) => {
-    const response = await axiosWithCredentials.post(`${HTTP_SERVER}/api/users/signin`, userData);
+    const response = await axiosWithCredentials.post(`/api/users/signin`, userData);
     return response.data;
 }
 export const signout = async () => {
-    const response = await axiosWithCredentials.post(`${HTTP_SERVER}/api/users/signout`);
+    const response = await axiosWithCredentials.post(`/api/users/signout`);
     return response.data;
 }
 export const profile = async () => {
-    const response = await axiosWithCredentials.get(`${HTTP_SERVER}/api/users/profile`);
+    const response = await axiosWithCredentials.get(`/api/users/profile`);
     return response.data;
 }
 export const findUserById = async (userId: string) => {
-    const response = await axios.get(`${HTTP_SERVER}/api/users/${userId}`);
+    const response = await axios.get(`/api/users/${userId}`);
     return response.data;
 }
 export const searchPlaces = async (query: string) => {
-    const response = await axios.post(`${HTTP_SERVER}/api/map/search`, {
+    const response = await axios.post(`/api/map/search`, {
         query
     });
     return response.data;
 };
 export const getPlaceDetails = async (placeId: string) => {
-    const response = await axios.get(`${HTTP_SERVER}/api/map/place/${placeId}`);
+    const response = await axios.get(`/api/map/place/${placeId}`);
     return response.data;
 }
 export const getPlaceTitle = async (placeId: string) => {
-    const response = await axios.get(`${HTTP_SERVER}/api/map/place/${placeId}/title`);
+    const response = await axios.get(`/api/map/place/${placeId}/title`);
     return response.data;
 }
 export const deletePost = async (postId: string) => {
-    const response = await axiosWithCredentials.delete(`${HTTP_SERVER}/api/posts/${postId}`);
+    const response = await axiosWithCredentials.delete(`/api/posts/${postId}`);
     return response.data;
 }
